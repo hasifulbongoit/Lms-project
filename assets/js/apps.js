1d39af36f72207ef12929cb61f8af13cad29b41b
hasifulbongoit <hasif.bongoitltd@gmail.com>
2022-12-08 17:17:05 +0600

    banner add

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,3 +1,20 @@
+
+// strciky header
+{
+    let mainHeader = document.getElementById("myHeader")
+    let sticky = mainHeader.offsetTop;
+
+    window.addEventListener("scroll", function(){
+        if(window.pageYOffset > sticky){
+            mainHeader.classList.add("sticky")
+        }
+        else{
+            mainHeader.classList.remove("sticky")
+        }
+    })
+
+}
+
 {
     let headerList = document.getElementById("header_nav_list")
     let headerListItem = headerList.children
@@ -19,5 +36,47 @@
         activeToggle.classList.toggle("activeToggle")
         mobileNav.classList.toggle("activeMobileNav")
     })
+
+}
+
+// banner slider
+
+{
+    let arrowLeft = document.getElementById("arrow_left")
+    let arrowRighr = document.getElementById("arrow_right")
+    let sliderItem = document.getElementsByClassName("banner_item")
+
+    let left = 0;
+    let sliderSize = 100;
+    let totalSize = sliderItem.length * sliderSize
+
+    // arrow left
+
+    arrowLeft.addEventListener("click", function(){
+        left -= sliderSize
+        if(left <= 0){
+            left = 0 
+        }
+        moveSliderItem(left)
+    })
+
+    // arrow right
+
+    arrowRighr.addEventListener("click", function(){
+        left += sliderSize
+        if(left >= totalSize){
+            left = totalSize - 100;
+        }
+        moveSliderItem(left)
+    })
+
+
+    // move function
+
+    function moveSliderItem(left){
+       ;[...sliderItem].forEach(moveItem => {
+          moveItem.style.left = - left + "%"
+       }); 
+    }
 
 }
